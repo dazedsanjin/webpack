2c8b3e524edf01d2ff5d3adaac1cc05e0daf94b0
shaoqing <shaoqing@dajiaok.com>
2021-11-04 18:15:19 +0800

    feat: '支持ES6+'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-import { helloworld } from './utils'
+// 箭头函数
+const add = (a, b) => a + b;
 
-document.write(helloworld())
+// Promise 对象
+const promise1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(add(1, 2));
+  }, 1000);
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(add(3, 4));
+  }, 1000);
+});
+
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(add(5, 6));
+  }, 1000);
+});
+
+Promise.all([promise1, promise2, promise3]).then(values => {
+  console.log(values); // [3, 7, 11]
+});
+
+// 实例方法：Array.prototype.includes()
+const arr = [1, 2, 3, 4, 5];
+console.log(arr.includes(3)); // true
+
+const root = document.getElementById('root');
+root.innerHTML = add(1, 3);
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-11-03 17:57:24
+ * @LastEditTime: 2021-11-04 17:52:52
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \webpack\webpack5\webpack.config.js
+ */
 const path = require('path')
 
 module.exports = {
@@ -5,6 +13,15 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
+      }
+    }]
   },
   mode: 'production'
 }
