2d91a492532dd49815902a62e165e151dc645ff6
gaoxin <1037396230@qq.com>
2021-11-16 00:24:02 +0800

    feat: 添加资源模块

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-15 09:50:44
- * @LastEditTime: 2021-11-15 10:18:06
+ * @LastEditTime: 2021-11-16 00:23:34
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \webpack\webpack5\webpack.dev.js
@@ -18,45 +18,42 @@ module.exports = {
   },
   module: {
     rules: [
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader'
-      }
-    },
-    {
-      test: /\.vue$/,
-      loader: 'vue-loader'
-    }, 
-    {
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        'css-loader'
-      ]
-    }, 
-    {
-      test: /\.scss$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }, 
-    {
-      test: /.(png|jpg|gif|jpeg)$/,
-      use: ['file-loader']
-    }, 
-    {
-      test: /\.(woff|woff2|eot|ttf|otf)$/,
-      use: [{
-        loader: 'url-loader',
-        options: {
-          limit: 10240
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
         }
-      }]
-    }]
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /.(png|jpg|gif|jpeg)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: '[name][hash:8].[ext]'
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        type: 'asset/inline',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024
+          }
+        }
+      }
+    ]
   },
   mode: 'development',
   plugins: [
--- webpack.prod.js
+++ webpack.prod.js
@@ -33,25 +33,17 @@ module.exports = {
       },
       {
         test: /.(png|jpg|gif|jpeg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name][hash:8].[ext]'
-            }
-          }
-        ]
+        type: 'asset/resource',
+        generator: {
+          filename: '[name][hash:8].[ext]'
+        }
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name][hash:8].[ext]'
-            }
-          }
-        ]
+        type: 'asset/resource',
+        generator: {
+          filename: '[name][hash:8].[ext]'
+        }
       }
     ]
   },
