bf16884d1b16caae0c4329852a4d3f434507ca0c
shaoqing <shaoqing@dajiaok.com>
2021-12-24 15:33:41 +0800

    feat: 基础库的分离

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-
 import Vue from 'vue'
 import App from './App.vue'
 export default new Vue({
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
--- webpack.prod.js
+++ webpack.prod.js
@@ -86,5 +86,8 @@ module.exports = {
   ],
   optimization: {
     minimizer: [new CssMinimizerPlugin()]
+  },
+  externals: {
+    vue: 'Vue'
   }
 }
